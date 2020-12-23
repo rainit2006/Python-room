f3771a7ead92fe2152908907889f0480d30e732e
rainit2006 <rainit2006@gmail.com>
2020-12-23 19:20:17 +0900

    Update test_leak.py

--- Python-Project/test_leak.py
+++ Python-Project/test_leak.py
@@ -4,3 +4,5 @@ access_key="112343adareaa"
 
 access_token="xadfafewaafas"
 
+race|human attribute|human analysis|human analytics|minority|Uyghur|Ethnic|人種
+|民族|国籍|ウイグル|Weizu|维族|人間の属性|人間の分析|human attribute|human analysis|人的属性|人的分析|种族|少数民族|维吾尔|民族
